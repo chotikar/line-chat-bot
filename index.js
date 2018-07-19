fb3c0fdf72237366f5e76cab96a7a24535649c25
chotikar <mookintention@gmail.com>
2018-07-19 15:14:23 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express')
-const bodyParser = require('body-parser')
-const request = require('request')
+// const bodyParser = require('body-parser')
+// const request = require('request')
 const app = express()
 const port = process.env.PORT || 4000
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+// app.use(bodyParser.urlencoded({ extended: false }))
+// app.use(bodyParser.json())
 // app.post('/webhook', (req, res) => {
 //     let reply_token = req.body.events[0].replyToken
 //     reply(reply_token)
