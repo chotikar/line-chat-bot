942228c3784072c69b4255e5582809878e617eb8
chotikarP <chotikar_p@createlcom.com>
2018-07-20 11:06:51 +0700

    edit for mat

--- index.js
+++ index.js
@@ -21,17 +21,17 @@ app.listen(port)
 function reply(reply_token, msg,req) {
     var status = 'false'
     let eventtype = req.body.events[0].type
-    let sourceType = req.body.events[0].source.type
-    let reply_token = req.body.events[0].replyToken
+    let sourcetype = req.body.events[0].source.type
+    let replytoken = req.body.events[0].replyToken
     // if ((eventtype === 'join' && (sourceType === 'group' || sourceType === 'room')) || (eventtype === 'message' && sourceType === 'user'){
-        status = 'true'
+    //     status = 'true'
     // } 
 
     let body = JSON.stringify({
         replyToken: reply_token,
         messages: [{
             type: 'text',
-            text: status + msg
+            text: eventtype+sourcetype + replytoken +status + msg
         }]
     })
     request.post({
