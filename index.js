22607f814602167359232f5784485b6815616b9c
chotikarP <chotikar_p@createlcom.com>
2018-07-20 10:46:17 +0700

    edit for mat

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json())
 app.post('/webhook', (req, res) => {
     let reply_token = req.body.events[0].replyToken
     let msg = req.body.events[0].message.text
-    reply(reply_token, JSON.stringify(req))
+    reply(reply_token, JSON.stringify(req.body.events[0]))
     res.sendStatus(200)
 })
 app.listen(port)
@@ -36,35 +36,12 @@ function reply(reply_token, msg) {
     });
 }
 
-function requestCommand(roomid, roomtype, personid) {
-  var options = {
-    url: 'http://10.0.4.115:80/line/command',
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    json: {
-      "roomId": roomid,
-      "roomType": roomtype,
-      "personId": personid
-    }
-  }
-
-  request(options, function (error, response, body) {
-    if (response.statusCode == 200) {
-      console.log(body)
-    }
-  })
-}
-
 function requestMessage(roomid, textmessage, personid) {
   var options = {
-    url: 'http://10.0.4.115/line/message',
+    url: 'http://203.154.57.171/line/message',
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
-    json: {
-      'personId': personid,
-      'roomId': roomid,
-      'text': textmessage
-    }
+    json: { req.body.events[0]}
   }
 
   request(options, function (error, response, body) {
@@ -77,75 +54,12 @@ function requestMessage(roomid, textmessage, personid) {
 function checkEvent(req) { 
     let eventtype = req.body.events[0].type
     let sourceType = req.body.events[0].source.type
-    if (eventtype === 'join' && (sourceType === 'group' || sourceType === 'room')){
-        let room_id = req.body.events[0]
-        let room_type = req.body.events[0]
-        let person_id = req.body.events[0]
-        requestCommand(room_id, room_type, person_id)
+    let reply_token = req.body.events[0].replyToken
+    if ((eventtype === 'join' && (sourceType === 'group' || sourceType === 'room')) || (eventtype === 'message' && sourceType === 'user'){
+        
+        requestMessage(room_id, room_type, person_id)
     } 
 
 }
-
-
-
-// const express = require('express')
-// const bodyParser = require('body-parser')
-// const request = require('request')
-// const app = express()
-// const port = process.env.PORT || 4000
-
-// let headers = {
-//         'Content-Type': 'application/json',
-//         'Authorization': 'Bearer OXFKQOlc4tKKxW6PPX8dFZCJa5bzfeE8BhtVjNzsppgHbnfWyPP6y3FGPfsrxDb5BqXpTC6HriZPJa77foj5wICauaoTexnfvX3DNVSnj99kFgHgTbwBHKgWsuWET0ln0R27DnP3Hin+vZx9wwqr0QdB04t89/1O/w1cDnyilFU='
-//     }
-
-// app.use(bodyParser.urlencoded({ extended: false }))
-// app.use(bodyParser.json())
-// app.post('/webhook', (req, res) => {
-//     let reply_token = req.body.events[0].replyToken
-//     let msg = req.body.events[0].message.text
-//     reply(reply_token, JSON.stringify(req.body))
-//     res.sendStatus(200)
-// })
-// app.listen(port)
-// function reply(reply_token, msg) {
-    
-//     let body = JSON.stringify({
-//         replyToken: reply_token,
-//         messages: [{
-//             type: 'text',
-//             text: msg
-//         }]
-//     })
-//     request.post({
-//         url: 'https://api.line.me/v2/bot/message/reply',
-//         headers: headers,
-//         body: body
-//     }, (err, res, body) => {
-//         console.log('status = ' + res.statusCode);
-//     });
-// }
-
-
-// function seperateMessageType(reply_token, req) {
-//     let messageType = req.body.events[0].message.type
-//     switch(messageType){
-//         case "text":
-//         reply(reply_token, "1." + messageType)
-//         break;
-//         case "image":
-//         reply(reply_token, "2." + messageType)
-//         break;
-//         case "sticker":
-//         reply(reply_token, "3." + messageType)
-//         break;
-//         case "location"
-//         reply(reply_token, "4." + messageType)
-//         break;
-//         default://audio
-//         reply(reply_token, "5." + messageType)
-//         break;
-//     }
-// } 
 
 
